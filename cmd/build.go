1aa8ee3b089b78f65bd2d2682201c5f2b4461e3d
CyberL1 <cyber.hf18@gmail.com>
2025-08-15 00:12:06 +0200

    feat(cmd): add `build` command

---
+++ cmd/build.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"fmt"
+	"html-cli/constants"
+	"html-cli/utils"
+	"io/fs"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(buildCmd)
+	utils.LoadConfig(".")
+
+	buildCmd.Flags().StringVarP(&constants.Config.Build.Directory, "out", "o", "build", "Output directory")
+}
+
+var buildCmd = &cobra.Command{
+	Use:   "build",
+	Short: "Build html",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := build("."); err != nil {
+			fmt.Println("Build failed:", err)
+			return
+		}
+
+		fmt.Println("\nBuild complete")
+	},
+}
+
+func build(directory string) error {
+	dirContents, err := fs.ReadDir(os.DirFS(directory), ".")
+	if err != nil {
+		return err
+	}
+
+	for _, file := range dirContents {
+		if file.Name() == "html-cli.json" || (file.IsDir() && file.Name() == constants.Config.Build.Directory) {
+			continue
+		}
+
+		if file.IsDir() {
+			build(filepath.Join(directory, file.Name()))
+		} else {
+			fmt.Println("Building:", filepath.Join(directory, file.Name()))
+
+			fileContents, err := os.ReadFile(filepath.Join(directory, file.Name()))
+			if err != nil {
+				return err
+			}
+
+			if filepath.Ext(file.Name()) == ".html" {
+				fileContents = utils.ApplyBoilerplate(fileContents, false)
+			}
+
+			os.MkdirAll(filepath.Join(constants.Config.Build.Directory, directory), 0775)
+			os.WriteFile(filepath.Join(constants.Config.Build.Directory, directory, file.Name()), fileContents, 0644)
+		}
+	}
+
+	return nil
+}
