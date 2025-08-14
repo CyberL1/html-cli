6f0755a46cf275c4414ea34a25024aecd70a7e29
CyberL1 <cyber.hf18@gmail.com>
2025-08-15 00:13:29 +0200

    feat(types): add  config for `build`

--- types/Config.go
+++ types/Config.go
@@ -1,10 +1,14 @@
 package types
 
 type Config struct {
-	Dev ConfigDev `json:"dev"`
+	Dev   ConfigDev   `json:"dev"`
+	Build ConfigBuild `json:"build"`
 }
 
 type ConfigDev struct {
-	Root string `json:"root"`
 	Port uint16 `json:"port"`
+}
+
+type ConfigBuild struct {
+	Directory string `json:"directory"`
 }
