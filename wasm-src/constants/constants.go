c0ccdd75af639f2c6de2693e877225fc73ea3ae1
CyberL1 <cyber.hf18@gmail.com>
2025-08-18 08:35:24 +0200

    chore: update the source

--- wasm-src/constants/constants.go
+++ wasm-src/constants/constants.go
@@ -6,3 +6,12 @@ var (
 	Version string
 	Config  types.Config
 )
+
+var DefaultConfig = types.Config{
+	Dev: types.ConfigDev{
+		Port: 8080,
+	},
+	Build: types.ConfigBuild{
+		Directory: "build",
+	},
+}
